26300b725b283efc88e8b9758a48c2a2f0e514a4
sezRR <sezer.tetik2005@gmail.com>
2024-08-14 14:34:50 +0300

    🎇 Added simple redis pub/sub examples.

---
+++ messaging/PubSub.Redis.Example/Redis.PubSub.Publisher/Program.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+
+var redisConnection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+ISubscriber subscriber = redisConnection.GetSubscriber();
+
+while (true)
+{ 
+    Console.Write("Message: ");
+
+    string message = Console.ReadLine()!;
+    await subscriber.PublishAsync("mychannel", message);
+}
---
+++ messaging/PubSub.Redis.Example/Redis.PubSub.Subscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+var redisConnection = await ConnectionMultiplexer.ConnectAsync("localhost:6379");
+
+ISubscriber subscriber = redisConnection.GetSubscriber();
+
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.WriteLine($"{channel} -> {message}");
+});
+
+Console.ReadLine();
